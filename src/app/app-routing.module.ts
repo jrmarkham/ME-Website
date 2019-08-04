6305c91d39194c9c77b692b55e69d016337eb4d9
John Markham <me@markhamenterprises.com>
2019-08-04 14:41:47 -0400

    style updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {NgModule, OnInit} from '@angular/core';
+import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RouteComponent} from './route/route.component';
 
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ShareDataService} from '../service/share-data.service';
 import {Router} from '@angular/router';
-import {isNumeric} from 'tslint';
-import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-content',
